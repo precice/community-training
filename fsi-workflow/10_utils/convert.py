50e6ef4ddf1f1474a1e300bb66cad464c7d8e76f
Claudio Caccia <c.caccia@libero.it>
2024-08-06 08:03:06 +0200

    move solid simulation before fluid mesh

--- fsi-workflow/10_utils/convert.py
+++ fsi-workflow/10_utils/convert.py
@@ -1,9 +1,9 @@
 import logging
 import ccx2paraview.ccx2paraview as ccx2p
 
-name = '../03_solidSimulation/solution/solidModel.frd'
+name = '../03_solidSimulation/solution/staticModel.frd'
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %/message)s")
-c = ccx2p.Converter(name, ["vtu"])
+c = ccx2p.Converter(name, ["vtk"])
 c.run()
 
