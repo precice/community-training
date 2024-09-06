f2cd178c62feb7acc54d1e165126b4298017fef1
Claudio Caccia <c.caccia@libero.it>
2024-09-06 10:02:45 +0200

    change plot convergence and diplacement to match file location

--- fsi-workflow/05_FSI/solution/FSI_water/plotDisplacement.sh
+++ fsi-workflow/05_FSI/solution/FSI_water/plotDisplacement.sh
@@ -8,8 +8,8 @@ set ylabel 'Y-Displacement [m]'
 # set style line 1 lt 2 lw 6
 # set linestyle  2 lt 2 lc 1 # red-dashed
 # set linestyle  1 lt 2 lc 1 # red-dashed
-plot "./precice-Solid-watchpoint-tip.log" using 1:9 title "tip" with lines, \
-     "./precice-Solid-watchpoint-tip-trailing.log" using 1:9 title "tip TE" with lines, \
-    "./precice-Solid-watchpoint-tip-leading.log" using 1:9 title "tip LE" with lines     
+plot "./Solid/precice-Solid-watchpoint-tip.log" using 1:6 title "tip" with lines, \
+     "./Solid/precice-Solid-watchpoint-tip-trailing.log" using 1:6 title "tip TE" with lines, \
+    "./Solid/precice-Solid-watchpoint-tip-leading.log" using 1:6 title "tip LE" with lines     
 EOF
 
