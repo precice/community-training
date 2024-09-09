7bf3f46e1986d9478ca1e1aa5f93a18da0cd178f
Claudio Caccia <c.caccia@libero.it>
2024-09-09 11:48:09 +0200

    added foldest to with empty files as placehoders for simulation results

---
+++ fsi-workflow/04_fluidSimulation/solution_water_Re50000/plotCoefficients.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('./Fluid/postProcessing/forceCoeffs_object/0/coefficient.dat', comments='#')
+
+fs = 36
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(20, 16))
+ax0.plot(data[:, 0], data[:, 1], label='Cd', lw=4)
+# ax0.set_title('Amplitude', fontsize=fs)
+ax0.set_xlabel(r'iteration', fontsize=fs)
+ax0.set_ylabel(r'$C_D$', fontsize=fs)
+# ax0.legend(fontsize=fs-2,loc=2)
+ax0.set_xlim([0, 500])
+# ax0.set_ylim([1, 1.7])
+ax0.grid()
+
+ax1.plot(data[:, 0], data[:, 4], label='Cd', lw=4)
+# ax0.set_title('Amplitude', fontsize=fs)
+ax1.set_xlabel(r'iteration', fontsize=fs)
+ax1.set_ylabel(r'$C_L$', fontsize=fs)
+# ax0.legend(fontsize=fs-2,loc=2)
+ax1.set_xlim([0, 500])
+# ax0.set_ylim([1, 1.7])
+ax1.grid()
