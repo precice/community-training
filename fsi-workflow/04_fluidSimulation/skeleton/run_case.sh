23a35cd1de5baa5bf493493b4b262316a61ff752
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-09-11 16:51:54 +0200

    Review step 4 (fluid simulation)

--- fsi-workflow/04_fluidSimulation/skeleton/run_case.sh
+++ fsi-workflow/04_fluidSimulation/skeleton/run_case.sh
@@ -5,7 +5,7 @@ cd Fluid
 cp -r 0.orig 0
 
 decomposePar
-#mpirun -np 8 renumberMesh -overwrite -parallel | tee log.renumberMesh
-mpirun -np 8 simpleFoam -parallel | tee log.solver
+#mpirun -np 8 -oversubscribe renumberMesh -overwrite -parallel | tee log.renumberMesh
+mpirun -np 8 -oversubscribe simpleFoam -parallel | tee log.solver 2>&1
 reconstructPar -latestTime
 
