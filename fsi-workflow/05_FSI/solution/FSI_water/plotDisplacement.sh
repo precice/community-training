4c371f42063a9168b9079e33a015df1f38635175
Claudio Caccia <c.caccia@libero.it>
2025-07-14 13:31:54 +0200

    update skeleton and solution case for solid part in FSI to allow root flap

--- fsi-workflow/05_FSI/solution/FSI_water/plotDisplacement.sh
+++ fsi-workflow/05_FSI/solution/FSI_water/plotDisplacement.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 gnuplot -p << EOF
 set grid
-set title 'Displacement of the wing Tip'
+set title 'Displacement of the wing Root and Tip'
 set xlabel 'Time [s]'
 set ylabel 'Y-Displacement [m]'
 # set style line 1 lt 2 lw 6
 # set style line 1 lt 2 lw 6
 # set linestyle  2 lt 2 lc 1 # red-dashed
 # set linestyle  1 lt 2 lc 1 # red-dashed
-plot "./Solid/precice-Solid-watchpoint-tip.log" using 1:6 title "tip" with lines, \
+plot "./Solid/precice-Solid-watchpoint-root.log" using 1:6 title "root" with lines, \
+     "./Solid/precice-Solid-watchpoint-tip.log" using 1:6 title "tip" with lines, \
      "./Solid/precice-Solid-watchpoint-tip-trailing.log" using 1:6 title "tip TE" with lines, \
-    "./Solid/precice-Solid-watchpoint-tip-leading.log" using 1:6 title "tip LE" with lines     
+     "./Solid/precice-Solid-watchpoint-tip-leading.log" using 1:6 title "tip LE" with lines     
 EOF
 
